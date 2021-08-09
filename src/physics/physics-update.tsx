e6866b69adb142a81a51901fc2876688d798243b
notrabs <bastiknownasseb@gmail.com>
2021-08-09 13:17:39 +0200

    skip unneccesary copies

--- src/physics/physics-update.tsx
+++ src/physics/physics-update.tsx
@@ -51,6 +51,8 @@ export function PhysicsUpdate({
         Atomics.load(sharedBuffers.rigidBodies.headerIntArray, 0) ===
           BufferState.READY)
     ) {
+      const lastLastTickMs = physicsPerformanceInfoRef.current.lastTickMs;
+
       physicsPerformanceInfoRef.current.lastTickMs =
         sharedBuffers.rigidBodies.headerFloatArray[1];
       physicsPerformanceInfoRef.current.lastTickTime =
@@ -61,61 +63,64 @@ export function PhysicsUpdate({
         fn!();
       }
 
-      for (let i = 0; i < uuids.length; i++) {
-        const uuid = uuids[i];
-        const type = bodyOptions[uuid].type
-          ? bodyOptions[uuid].type
-          : BodyType.DYNAMIC;
-        const object3D = object3Ds[uuid];
-        if (type === BodyType.DYNAMIC) {
-          matrix.fromArray(
-            sharedBuffers.rigidBodies.objectMatricesFloatArray,
-            uuidToIndex[uuid] * BUFFER_CONFIG.BODY_DATA_SIZE
-          );
-
-          inverse.copy(object3D.parent!.matrixWorld).invert();
-          transform.multiplyMatrices(inverse, matrix);
-          transform.decompose(object3D.position, object3D.quaternion, scale);
-        } else {
-          // sharedBuffers.rigidBodies.objectMatricesFloatArray.set(
-          //   object3D.matrixWorld.elements,
-          //   uuidToIndex[uuid] * BUFFER_CONFIG.BODY_DATA_SIZE
-          // );
-        }
-
-        // print velocities
-        // console.log(
-        //   uuid,
-        //   objectMatricesFloatArray[indexes[uuid] * BUFFER_CONFIG.BODY_DATA_SIZE + 16],
-        //   objectMatricesFloatArray[indexes[uuid] * BUFFER_CONFIG.BODY_DATA_SIZE + 17]
-        // );
-
-        // print coliisions
-        // const collisions = [];
-        // for (let j = 18; j < 26; j++) {
-        //   const collidingIndex = objectMatricesIntArray[uuidToIndex[uuid] * BUFFER_CONFIG.BODY_DATA_SIZE + j];
-        //   if (collidingIndex !== -1) {
-        //     collisions.push(IndexToUuid[collidingIndex]);
-        //   }
-        // }
-        // console.log(uuid, collisions);
-      }
-
-      for (const softBodyBuffers of sharedBuffersRef.current.softBodies) {
-        const softBodyMesh = softBodies[softBodyBuffers.uuid];
-
-        if (softBodyMesh) {
-          if (!isSharedArrayBufferSupported) {
-            softBodyMesh.geometry.attributes.position.set(
-              softBodyBuffers.vertexFloatArray
+      // Skip copy if the physics worker didnt update
+      if (lastLastTickMs !== physicsPerformanceInfoRef.current.lastTickMs) {
+        for (let i = 0; i < uuids.length; i++) {
+          const uuid = uuids[i];
+          const type = bodyOptions[uuid].type
+            ? bodyOptions[uuid].type
+            : BodyType.DYNAMIC;
+          const object3D = object3Ds[uuid];
+          if (type === BodyType.DYNAMIC) {
+            matrix.fromArray(
+              sharedBuffers.rigidBodies.objectMatricesFloatArray,
+              uuidToIndex[uuid] * BUFFER_CONFIG.BODY_DATA_SIZE
             );
-            softBodyMesh.geometry.attributes.normal.set(
-              softBodyBuffers.normalFloatArray
-            );
+
+            inverse.copy(object3D.parent!.matrixWorld).invert();
+            transform.multiplyMatrices(inverse, matrix);
+            transform.decompose(object3D.position, object3D.quaternion, scale);
+          } else {
+            // sharedBuffers.rigidBodies.objectMatricesFloatArray.set(
+            //   object3D.matrixWorld.elements,
+            //   uuidToIndex[uuid] * BUFFER_CONFIG.BODY_DATA_SIZE
+            // );
           }
 
-          softBodyMesh.geometry.attributes.position.needsUpdate = true;
-          softBodyMesh.geometry.attributes.normal.needsUpdate = true;
+          // print velocities
+          // console.log(
+          //   uuid,
+          //   objectMatricesFloatArray[indexes[uuid] * BUFFER_CONFIG.BODY_DATA_SIZE + 16],
+          //   objectMatricesFloatArray[indexes[uuid] * BUFFER_CONFIG.BODY_DATA_SIZE + 17]
+          // );
+
+          // print coliisions
+          // const collisions = [];
+          // for (let j = 18; j < 26; j++) {
+          //   const collidingIndex = objectMatricesIntArray[uuidToIndex[uuid] * BUFFER_CONFIG.BODY_DATA_SIZE + j];
+          //   if (collidingIndex !== -1) {
+          //     collisions.push(IndexToUuid[collidingIndex]);
+          //   }
+          // }
+          // console.log(uuid, collisions);
+        }
+
+        for (const softBodyBuffers of sharedBuffersRef.current.softBodies) {
+          const softBodyMesh = softBodies[softBodyBuffers.uuid];
+
+          if (softBodyMesh) {
+            if (!isSharedArrayBufferSupported) {
+              softBodyMesh.geometry.attributes.position.set(
+                softBodyBuffers.vertexFloatArray
+              );
+              softBodyMesh.geometry.attributes.normal.set(
+                softBodyBuffers.normalFloatArray
+              );
+            }
+
+            softBodyMesh.geometry.attributes.position.needsUpdate = true;
+            softBodyMesh.geometry.attributes.normal.needsUpdate = true;
+          }
         }
       }
 
