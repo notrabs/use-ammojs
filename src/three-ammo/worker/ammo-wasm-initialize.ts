58443e1b545c1e328632dd8f4116164a298509d4
notrabs <bastiknownasseb@gmail.com>
2021-09-03 13:03:19 +0200

    switch to ammo.js as submodule

--- src/three-ammo/worker/ammo-wasm-initialize.ts
+++ src/three-ammo/worker/ammo-wasm-initialize.ts
@@ -1,7 +1,7 @@
 // TODO: figure out why rollup doesn't like the file out of the ammo.js repo and throws a missing "default export" error
 // current workaround is to append "export default Ammo;" to the end of the file
-import Ammo from "../lib/builds/ammo.wasm.js";
-import AmmoWasm from "../lib/builds/ammo.wasm.wasm";
+import Ammo from "../lib/ammo.js/builds/ammo.wasm.js";
+import AmmoWasm from "../lib/ammo.js/builds/ammo.wasm.wasm";
 
 export function initializeAmmoWasm(wasmUrl: string | undefined) {
   return Ammo.bind(self)({
@@ -17,7 +17,7 @@ export function initializeAmmoWasm(wasmUrl: string | undefined) {
 
 // non-wasm Ammo build for testing
 
-// import Ammo from "../lib/builds/ammo.js";
+// import Ammo from "../lib/ammo.js/builds/ammo.js";
 //
 // export function initializeAmmoWasm(wasmUrl: string | undefined) {
 //   return Ammo.bind(self)();
